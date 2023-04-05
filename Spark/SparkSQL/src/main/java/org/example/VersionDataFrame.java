b800bf6433766c48a2a6537fdaa090f8be87c5b3
Daabal Sokaina <48890714+sokainadaabal@users.noreply.github.com>
2023-04-05 02:42:22 +0000

    update

--- Spark/SparkSQL/src/main/java/org/example/VersionDataFrame.java
+++ Spark/SparkSQL/src/main/java/org/example/VersionDataFrame.java
@@ -1,5 +1,24 @@
 package org.example;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class VersionDataFrame {
 
+    public static void main(String[] args) {
+
+        Logger.getLogger("org").setLevel(Level.OFF);
+        SparkSession ss=SparkSession.builder().master("local[*]").appName("tp spark sql").getOrCreate();
+        Map<String,String> options=new HashMap<>();
+        options.put("driver","com.mysql.cj.jdbc.Driver");
+        options.put("url","jdbc:mysql://localhost:3306/DB_HOPITAL");
+        options.put("user","root");
+        options.put("password","");
+
+        
+    }
 }
