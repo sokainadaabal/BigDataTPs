cc3cee6add8f851e1f57fdf10e8c6b69f32ab9ff
Daabal Sokaina <48890714+sokainadaabal@users.noreply.github.com>
2023-04-05 03:10:38 +0000

    update

--- Spark/SparkSQL/src/main/java/org/example/VersionDataFrame.java
+++ Spark/SparkSQL/src/main/java/org/example/VersionDataFrame.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 import java.util.HashMap;
@@ -19,6 +21,22 @@ public class VersionDataFrame {
         options.put("user","root");
         options.put("password","");
 
-        
+         // query 1
+        Dataset<Row> df1=ss.read().format("jdbc").option("url","jdbc:mysql://localhost:3306/DB_HOPITAL")
+        .option("user","root").option("password","").option("dbtable","CONSULTATIONS").load();
+        df1.select("DATE_CONSULTATION").groupBy("DATE_CONSULTATION").count().show();
+
+        // query 2
+        Dataset<Row> df2_1=ss.read().format("jdbc").option("url","jdbc:mysql://localhost:3306/DB_HOPITAL")
+                .option("user","root").option("password","").option("dbtable","CONSULTATIONS").load();
+        Dataset<Row> df2_2=ss.read().format("jdbc").option("url","jdbc:mysql://localhost:3306/DB_HOPITAL")
+                .option("user","root").option("password","").option("dbtable","MEDECINS").load();
+        Dataset<Row> jointure1=df2_1.join(df2_2);
+        jointure1.printSchema();
+        jointure1.show();
+        jointure1.select("NOm","PRENOM").groupBy("NOM","PRENOM").count().show();
+        // query 3
+
+
     }
 }
